a6f1f98ad3bddaf7e96ed80b3f96d4e13a617a0a
qinghao@2dfire.com <qinghao@2dfire.com>
2017-06-08 22:58:34 +0800

    初始化项目

---
+++ src/main/java/com/wch/guava/Test.java
@@ -0,0 +1,41 @@
+package com.wch.guava;
+
+import java.util.ArrayList;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Multiset;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Function<String, Integer> lengthFunction = new Function<String, Integer>() {
+		    public Integer apply(String string) {
+		        return string.length();
+		    }
+		};
+		
+		lengthFunction.apply("你好啊") ;
+		
+		Predicate<String> allCaps = new Predicate<String>() {
+		    public boolean apply(String string) {
+		        return CharMatcher.JAVA_UPPER_CASE.matchesAllOf(string);
+		    }
+		};
+		java.util.List<String> strings = new ArrayList<String>() {
+			{
+				add("AB1231");
+				add("1231");
+				add("1231");
+				add("1231");
+			}
+		};
+		Multiset<Integer> lengths = HashMultiset.create(
+				 Iterables.transform(Iterables.filter(strings, allCaps), lengthFunction));
+		
+		System.out.println(lengths);
+	}
+}
