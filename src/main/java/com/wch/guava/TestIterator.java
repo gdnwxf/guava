06942541402482169b6bf1f91c919eea241d1f57
qinghao <qinghao@2dfire.com>
2017-06-09 15:49:26 +0800

    迭代器的使用

--- src/main/java/com/wch/guava/TestIterator.java
+++ src/main/java/com/wch/guava/TestIterator.java
@@ -5,11 +5,16 @@ import java.util.Iterator;
 import java.util.List;
 
 import com.google.common.collect.AbstractIterator;
+import com.google.common.collect.AbstractSequentialIterator;
 
 /**
  * 
  * @author qinghao
- *
+ * <pre>
+ * AbstractIterator继承了UnmodifiableIterator
+ * ，所以禁止实现remove()方法。
+ * 如果你需要支持remove()的迭代器，就不应该继承AbstractIterator。
+ * </pre>
  *
  */
 public class TestIterator {
@@ -33,8 +38,26 @@ public class TestIterator {
 	  while (skipNulls.hasNext()) {
 		  System.out.println(skipNulls.next().length());
 	  }
-	  
+	
+	  //当遇到null 的值的时候就会停止
+	  Iterator<Integer> powersOfTwo = new AbstractSequentialIterator<Integer>(1) { // 注意初始值1!
+		    protected Integer computeNext(Integer previous) {
+		        return (previous == 1 << 30) ? null : previous * 2;
+		    }
+		};
+		System.out.println("-------");
+		
+		while (powersOfTwo.hasNext()) {
+			
+			System.out.println(powersOfTwo.next());
+		}
+		
+		System.out.println(1 << 30);
+		
+		System.out.println(1 == 1 << 30);
 	}
+	
+	
 	/**
 	 * <pre>
 	 *  包装器跳过 null 值
