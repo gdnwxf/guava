729a3a18e061d93a5a068e350da60f4ece539205
qinghao <qinghao@2dfire.com>
2017-06-09 18:40:26 +0800

    TestCache

--- src/main/java/com/wch/guava/TestCache.java
+++ src/main/java/com/wch/guava/TestCache.java
@@ -1,5 +1,6 @@
 package com.wch.guava;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -8,6 +9,7 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.cache.RemovalListener;
 import com.google.common.cache.RemovalNotification;
+import com.google.common.cache.Weigher;
 import com.google.common.graph.Graph;
 import com.google.common.graph.GraphBuilder;
 import com.google.common.graph.ImmutableValueGraph;
@@ -46,14 +48,25 @@ public class TestCache {
 		 * 
 		 */
 		LoadingCache<String, Object> graphs = CacheBuilder.newBuilder()
-		        .maximumSize(1000)
+				//设置最大长度
+		        //.maximumSize(1000)
+		        //  设置写后失效
 		        .expireAfterWrite(10, TimeUnit.MINUTES)
+		        //   设置访问后失效
+		        .expireAfterAccess(10, TimeUnit.MINUTES)
 		        .removalListener(new RemovalListener<String, Object>() {
 					@Override
 					public void onRemoval(RemovalNotification<String, Object> notification) {
 						//处理删除缓存的 Key Value
 					}
 				} )
+		        // 设置权重值
+		        .maximumWeight(1<<30)
+		        .weigher(new Weigher<String, Object>() {
+		            public int weigh(String k, Object g) {
+		                return g.hashCode(); 
+		            }
+		        })
 		        .build(
 		            new CacheLoader<String, Object>() {
 		            	//Graph 提供有向图 和 无向图 
@@ -81,7 +94,32 @@ public class TestCache {
 		
 		
 		graphs.put("nihao", "你好");
-		
+
 		System.out.println(graphs.get("nihao"));
+		System.out.println(graphs.getIfPresent("xxx"));
+		// 
+		System.out.println(graphs.get("nihao1" ,
+				new Callable<Object>() {
+			/**
+			 * 基于异步的调用  如果没有key值存在 时候才掉的
+			 * <pre>
+			 * 不管有没有自动加载功能，都支持get(K, Callable<V>)方法。
+			 * 这个方法返回缓存中相应的值，或者用给定的Callable运算并把结果加入到缓存中。
+			 * 在整个加载方法完成前，缓存项相关的可观察状态都不会更改。
+			 * 这个方法简便地实现了模式"如果有缓存则返回；否则运算、缓存、然后返回"。
+			 * </pre>
+			 */
+			public Object call() throws Exception {
+				System.out.println("你的靠");
+				return new Object();
+			}
+			
+		})  );
+		
+		
+		
+		
+		
+		
 	}
 }
