a13a8d82c67eb376d8bac50c13774d79289e9a3c
qinghao <qinghao@2dfire.com>
2017-06-21 14:40:27 +0800

    unsafe的用法

--- src/main/java/com/wch/guava/TestCompareAndIncream.java
+++ src/main/java/com/wch/guava/TestCompareAndIncream.java
@@ -3,10 +3,15 @@ package com.wch.guava;
 import java.lang.reflect.Field;
 import sun.misc.Unsafe;
 
+/**
+ * unsafe用法
+ * @author qinghao
+ *
+ */
 public class TestCompareAndIncream {
 
-	private  volatile int a = 1;
- 
+	private static volatile int a = 1;
+	// private static final Unsafe unsafe = Unsafe.getUnsafe();
 
 	private static Unsafe getUnsafeInstance()
 			throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
@@ -15,30 +20,38 @@ public class TestCompareAndIncream {
 		return (Unsafe) theUnsafeInstance.get(Unsafe.class);
 	}
 
-	public static void main(String[] args) throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-	     final TestCompareAndIncream testCompareAndIncream  = new TestCompareAndIncream();
-		   new Thread( ) {
-	        	public void run() {
-	        		try {
-						Thread.sleep(3000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					};
-					testCompareAndIncream.a = 3;
-	        	};
-	        } .start();
-	        
-	   
-		 Unsafe unsafe = getUnsafeInstance();  
-		 
-		 Field appleField = TestCompareAndIncream.class.getDeclaredField("a");  
-		 long objectFieldOffset = unsafe.objectFieldOffset(appleField);
-	        System.out.println("Location of Apple: " + unsafe.staticFieldOffset(appleField));  
-	  
-	        unsafe.compareAndSwapInt(TestCompareAndIncream.class, objectFieldOffset, 3, 10);
-	        System.out.println(" 等待分隔符 ---- ");
-	        System.out.println( testCompareAndIncream.a);
-	        
+	public static void main(String[] args)
+			throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+		final TestCompareAndIncream testCompareAndIncream = new TestCompareAndIncream();
+		new Thread() {
+			public void run() {
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				;
+				System.out.println(" 设置成three ");
+				testCompareAndIncream.a = 3;
+				System.out.println(" 设置成 " + testCompareAndIncream.a);
+			};
+		}.start();
+
+		Unsafe unsafe = getUnsafeInstance();
+
+		Field appleField = TestCompareAndIncream.class.getDeclaredField("a");
+		long objectFieldOffset = unsafe.staticFieldOffset(appleField);
+		System.out.println("Location of Apple: " + unsafe.staticFieldOffset(appleField));
+		boolean compareAndSwapInt = false;
+		System.out.println("循环设置值 ");
+		do {
+			compareAndSwapInt = unsafe.compareAndSwapInt(TestCompareAndIncream.class, objectFieldOffset, 3, 10);
+			System.out.println(" 设置中 ");
+		} while (!compareAndSwapInt);
+
+		System.out.println("等待分隔符 ---- ");
+		System.out.println(testCompareAndIncream.a);
+
 	}
 }
